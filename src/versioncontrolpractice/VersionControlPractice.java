9bbc7e93899653837ea0389923f8ebc795a51b7f
jt24aaj <jt24aaj@herts.ac.uk>
2026-03-03 08:24:21 +0000

    change output message

--- src/versioncontrolpractice/VersionControlPractice.java
+++ src/versioncontrolpractice/VersionControlPractice.java
@@ -15,7 +15,7 @@ public class VersionControlPractice {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello version control");
+        System.out.println("version2- small improvement");
     }
     
 }
